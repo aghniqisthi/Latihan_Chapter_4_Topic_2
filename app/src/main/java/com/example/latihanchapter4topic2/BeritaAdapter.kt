b7b30238c24c5f74793239a2ac9179771b49b7e5
aghniqisthi <20082010113@student.upnjatim.ac.id>
2022-09-16 16:37:08 +0700

    Commit Perbaikan Scroll View

--- app/src/main/java/com/example/latihanchapter4topic2/BeritaAdapter.kt
+++ app/src/main/java/com/example/latihanchapter4topic2/BeritaAdapter.kt
@@ -1,10 +1,8 @@
 package com.example.latihanchapter4topic2
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.cardview.widget.CardView
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.latihanchapter4topic2.databinding.ItemBeritaBinding
